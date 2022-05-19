1b938c99942b896472a373ec142c5f36a32901bc
Luiz Felipe Alves Maia <luiz.felipeam@hotmail.com>
2022-05-19 19:12:31 -0300

    Adcionando comentarios nos arquivos de JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+/*Está linha escreve um linha com a tag <H1> dentro da DOM exbindo no HTML uma linha com o texto */
 document.write("<h1>Estamos executando o script.js</h1>");
+
+/*Essa linha executa uma linha  com o texto dentro do <body> do HTML*/
 document.write("Esse script está em um arquivo externo e é referenciado no header do HTML");
+
+/*Essa linha executa um comando mostrando um conjunto de string na console do navegador */
 console.log("Esse script está no cabeçalho do HTML");
+
+/*Essa linha executa um comando mostrando um conjunto de string na console do navegador */
 console.log("Primeiro");
+
+/*Essa linha executa um comando mostrando um conjunto de string na console do navegador */
 console.log("Segundo");
 
 // Comentário de uma linha
@@ -13,16 +22,23 @@ console.log("Segundo");
 */
 
 // Tipagem fraca
+
+/*Este conjunto de codigo executa uma iteração dentro da DOM, usando uma tag com quebra de paragrafo iterando o valor da variavel*/
 var teste = 1;
 document.write("<hr>O valor da variável teste é " + teste + "<hr>");
 teste = 'joao';
 document.write("O valor da variável teste agora é " + teste + "<hr>");
 
 // Variável global - criada sem necessidade de declaração
+
+/*Essa declaração usa uma variavel global que é mostrada na console do navegador */
 nome = 'joao';
 console.log(nome)
 
 // Formas de declaração de variáveis
+
+/*Este bloco de declarações, executa um comando na console do navegador, porém, mostrando o tipo 
+de cada variavel na console do navegador*/
 var   v1 = 5.25;
 let   v2 = null; 
 const v3 = 'teste'
@@ -34,26 +50,35 @@ console.log(typeof NaN); // tipo number
 console.log(typeof +Infinity); // tipo number
 console.log(typeof null); // tipo number
 const number = "1";
+
+
 // Operadores de comparação
+
+/*Este bloco executa na console do navegador uma comparação se os valores correspondem ao 
+seu tipo, retornando um valor booleano*/
 console.log(number == 1);
 // true
 console.log(number === 1);
 // false
 
+/*Executa um texto dentro do console do navegador */
 console.log('ele disse "vtnc"');
 
-// adicionar uma lista no body do HTML
+// adicionar uma lista no body do HTML, 
 var lista = ['Arroz', 'Feijão', 'carne','macarrão'];
 var listaUl = document.createElement('ul');
 var body = document.getElementsByTagName('body');
 
+/*O comando exibe os items da lista ordenada na console */
 console.log(listaUl);
 console.log(body);
 
 //alert("Vamos criar uma lista aqui!");
-
+/*Adciona a Lista no body da pagina */
 body[0].appendChild(listaUl);
 
+
+/*Este comando adciona os itens da Lista dentro da UL criada no body do HTML */
 for(var  i=0;i<lista.length;i++) {
     var liFor = document.createElement('li');
     var textoLi = document.createTextNode(lista[i]);
@@ -61,8 +86,10 @@ for(var  i=0;i<lista.length;i++) {
     listaUl.appendChild(liFor);
 }
 
+/*Escreve o texto entre "" no body avisando sobre a lista criada acima */
 document.write("A lista não ordenada <b>ACIMA</b> foi criada dinamicamente via JavaScript");
 
+/*Cria um objeto com atributos */
 var obj = {
     nome: "Joao",
     sobrenome: "Furtado",
@@ -71,14 +98,20 @@ var obj = {
     pessoaJuridica: true
 };
 
+/*Exibe os atributos do objeto criado  */
 console.log(obj);
+
+/*Exibe o texto mais a iteração com o atributo do objeto*/
 console.log("Salario: R$" + obj.salario);
 
+/*Cria um array lista com varios items de difereentes tipos, ou seja, um array com tipagem dinamica */
 var arr = [5, "JP", true, {teste1: 1, teste2: 2}, false];
+/*Exibe os itens dentro do array e o seu tipo */
 console.log(arr);
 console.log(arr[1]);
 console.log(typeof arr);
 
+/*Este comando faz uma comparação */
 if(arr[4]) {
     console.log("Entrou no teste do IF")
 }
--- js/script2.js
+++ js/script2.js
@@ -1,2 +1,5 @@
+
+/*Adciona uma tag H1 dentro do <body> da pagina e adcionando um novo texto */
 document.write("<h1>Estamos executando o script2.js</h1>");
+/*Adciona um texto dentro do <body> da pagina e adcionando um novo texto */
 document.write("Esse script está em um arquivo externo e é referenciado no final do body do HTML");
